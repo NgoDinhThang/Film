8bf4755250175eb7b6104104c2bd23500e035ee1
YordleKun <apple@japannoMacBook-Pro.local>
2021-08-16 07:54:40 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path')
-const exphbs  = require('express-handlebars');
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const exphbs = require('express-handlebars');
 const route = require('./routes');
 // const morgan = require('morgan')
 
 // app.use(morgan('combined'))
 
-app.engine('.hbs', exphbs({extname:'.hbs'}));
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-route(app)
+route(app);
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
